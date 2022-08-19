9574ccca9352e1a64f8b8ff14d0ef446a93f205c
vvidday <davids89034@gmail.com>
2022-08-19 15:56:25 +0800

    test: setup vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    include: './src/components/tests/unit/*.{test,spec}.{js,ts,jsx,tsx}'
+  },
   resolve:{
     alias:{
       '@' : path.resolve(__dirname, './src')
